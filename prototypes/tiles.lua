636873ea4d761b13b571b82d86c88a9fc7648079
Kyria <misc+gh@khon.dev>
2025-04-15 20:40:14 +0200

    feat: add some tiles for each platform level

---
+++ prototypes/tiles.lua
@@ -0,0 +1,83 @@
+local function create_tile(tile_info)
+    -- Deep copy the base concrete tile
+    local tile = table.deepcopy(data.raw["tile"]["concrete"])
+
+    -- Modify the tile properties
+    tile.name = tile_info.name
+    tile.group = "ground-artificial" -- Set the layer group
+    tile.tint = tile_info.tint -- Apply the tint to the tile
+    tile.type = "tile"
+    tile.layer = 75
+    tile.walking_speed_modifier = 1.5
+    tile.variant = nil
+    if not tile_info.buildable then
+        data:extend({tile})
+    else
+        tile.minable = {
+            mining_time = 0.1,
+            result = tile_info.name,
+            count = 1,
+        }
+
+        local item = table.deepcopy(data.raw["item"]["concrete"])
+        item.name = tile_info.name
+        item.stack_size = 50
+        item.icons = {
+            {
+                icon = data.raw["item"]["concrete"].icon,
+                icon_size = 64,
+                tint = tile_info.tint
+            }
+        }
+        item.place_as_tile = {
+            result = tile_info.name,
+            condition = {layers = {ground_tile = true}},
+            condition_size = 1,
+            tile_condition = {"water", "deepwater"}
+        }
+
+        local item_recipe = {
+            type = "recipe",
+            name = tile_info.name,
+            icons = {
+                {
+                    icon = data.raw["item"]["concrete"].icon,
+                    icon_size = 64,
+                    tint = tile_info.tint
+                }
+            },
+            category = "advanced-crafting",
+            ingredients = tile_info.ingredients or {{type="item", name="concrete", amount=100}},
+            results = {{type = "item", name = tile_info.name, amount = 1}},
+            auto_recycle = true
+        }
+
+        data:extend({tile, item, item_recipe})
+    end
+end
+
+create_tile {
+    name = "warp-platform",
+    tint = defines.color.slategrey,
+    buildable = false,
+}
+create_tile {
+    name = "factory-platform",
+    tint = defines.color.lightgoldenrodyellow,
+    buildable = true,
+}
+create_tile {
+    name = "energy-platform",
+    tint = defines.color.lightsteelblue,
+    buildable = true,
+}
+create_tile {
+    name = "mining-platform",
+    tint = defines.color.rosybrown,
+    buildable = true,
+}
+create_tile {
+    name = "spacehub-platform",
+    tint = defines.color.lightskyblue,
+    buildable = true,
+}
