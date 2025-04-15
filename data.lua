30885e8dd3de569ab8ffb2036e29b922f2c0834d
Kyria <misc+gh@khon.dev>
2025-04-15 20:41:13 +0200

    feat: add prototypes and settings files

---
+++ data.lua
@@ -0,0 +1,33 @@
+require 'lib.defines'
+
+require 'prototypes.planet.neo-nauvis'
+require 'prototypes.teleport'
+require 'prototypes.tiles'
+require 'prototypes.technologies.technology'
+
+
+data:extend({
+    {
+        type = "sprite",
+        name = "warp-toggle-icon",
+        priority = "extra-high-no-scale",
+        layers = {
+            {
+                filename = data.raw["planet"]["nauvis"].icon,
+                tint = defines.color.royalblue,
+                width = 64,
+                height = 64,
+                scale = 0.5
+            },
+            {
+                filename = "__base__/graphics/icons/info.png",
+                width = 64,
+                height = 64,
+                scale = 0.2,
+                shift = {10,10},
+                tint = defines.color.lightsteelblue,
+            },
+        },
+        flags = {"gui-icon"},
+    }
+})
