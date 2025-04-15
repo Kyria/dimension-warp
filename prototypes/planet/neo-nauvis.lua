5b751bbd7f6d1cd4dc2caef41b651e12042f007d
Kyria <misc+gh@khon.dev>
2025-04-15 20:38:58 +0200

    feat: add neo-nauvis

---
+++ prototypes/planet/neo-nauvis.lua
@@ -0,0 +1,32 @@
+local asteroid_util = require("__space-age__.prototypes.planet.asteroid-spawn-definitions")
+local nauvis = data.raw["planet"]["nauvis"]
+local neo_nauvis = table.deepcopy(nauvis)
+
+neo_nauvis.name = "neo-nauvis"
+neo_nauvis.order = "a[neo-nauvis]"
+neo_nauvis.type = "planet"
+neo_nauvis.subgroup = "planets"
+neo_nauvis.orbit = {
+    parent = {name = "star", type = "space-location"},
+    distance = 65,
+    orientation = 0.28
+}
+neo_nauvis.draw_orbit = false
+
+neo_nauvis.icons = {{
+        icon = nauvis.icon,
+        tint = defines.color.royalblue
+}}
+neo_nauvis.starmap_icons = {{
+        icon = nauvis.starmap_icon,
+        icon_size = 512,
+        tint = defines.color.royalblue
+}}
+neo_nauvis.asteroid_spawn_influence = 1
+neo_nauvis.asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.nauvis_vulcanus, 0.1)
+
+-- remove some fields nt wanted by planetslib
+neo_nauvis.distance = nil
+neo_nauvis.orientation = nil
+
+PlanetsLib:extend({neo_nauvis})
