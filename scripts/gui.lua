1982c6e3f8807c6467051c15d47e6308c29f93fa
Kyria <misc+gh@khon.dev>
2025-06-01 23:10:06 +0200

    feat: update gui to make use of parameters in locals. Also beautify it a bit

--- scripts/gui.lua
+++ scripts/gui.lua
@@ -2,12 +2,14 @@
 ------------------------------------------------------------
 dw.gui = dw.gui or {}
 
-dw.gui.get_warp_toggle_button = function(player)
+dw.gui.set_warp_toggle_buttons = function(player)
     local buttonflow = mod_gui.get_button_flow(player)
-    local button = buttonflow.warp_toggle or buttonflow.add({type = "sprite-button", name = "warp_toggle", sprite = "warp-toggle-icon"})
-    button.visible = true
-    button.tooltip = {"dw-messages.gui-button-tooltip"}
-    return button
+    local button_info = buttonflow.warp_toggle or buttonflow.add({type = "sprite-button", name = "warp_toggle", sprite = "warp-toggle-icon"})
+    local button_action = buttonflow.warp_action_toggle or buttonflow.add({type = "sprite-button", name = "warp_action_toggle", sprite = "warp-actions-toggle-icon"})
+    button_info.visible = true
+    button_info.tooltip = {"dw-messages.gui-button-tooltip"}
+    button_action.visible = true
+    button_action.tooltip = {"dw-messages.gui-button-tooltip"}
 end
 
 dw.gui.get_warp_frame = function(player)
@@ -17,35 +19,18 @@ dw.gui.get_warp_frame = function(player)
         warp_frame = frameflow.add({type = "flow", name = "warp_frame", direction = "vertical"})
         warp_frame.visible = false
         warp_frame.style.padding = {5, 0, 0, 5}
-        local surfaceflow = warp_frame.add({type = "flow", name="surface", direction = "horizontal"})
-        local dimensionflow = warp_frame.add({type = "flow", name="dimension", direction = "horizontal"})
-        local surface_time = warp_frame.add({type = "flow", name="surface_time", direction = "horizontal"})
-        local surface_evolution = warp_frame.add({type = "flow", name="surface_evolution", direction = "horizontal"})
-        local warpflow = warp_frame.add({type = "flow", name="warp_timer", direction = "horizontal"})
-        local manualwarpflow = warp_frame.add({type = "flow", name="warp_timer_manual", direction = "horizontal"})
+        local surfaceflow = warp_frame.add({type = "label", name = "surface", caption = {"dw-gui.planet", "Nauvis"}})
+        local dimensionflow = warp_frame.add({type = "label", name = "dimension", caption = {"dw-gui.dimension", "0"}})
+        local surface_time = warp_frame.add({type = "label", name = "surface_time", caption = {"dw-gui.planet_clock", "0"}})
+        local surface_evolution = warp_frame.add({type = "label", name = "surface_evolution", caption = {"dw-gui.planet_evolution", "0"}})
+        local warpflow = warp_frame.add({type = "label", name = "warp_timer", caption = {"dw-gui.autowarp_timer", "0"}})
+        local manualwarpflow = warp_frame.add({type = "label", name = "warp_timer_manual", caption = {"dw-gui.manualwarp_timer", "0"}})
 
-        surfaceflow.add{type="label", caption="Planet: "}
-        surfaceflow.add{type="label", name="variable", caption="Nauvis"}
         surfaceflow.visible = storage.nauvis_lab_exploded or false
-
-        dimensionflow.add{type="label", caption="Dimension: "}
-        dimensionflow.add{type="label", name="variable", caption="0"}
         dimensionflow.visible = storage.nauvis_lab_exploded or false
-
-        surface_time.add{type="label", caption="Planet time: "}
-        surface_time.add{type="label", name="variable", caption="0"}
         surface_time.visible = storage.nauvis_lab_exploded or false
-
-        surface_evolution.add{type="label", caption="Planet evolution: "}
-        surface_evolution.add{type="label", name="variable", caption="0"}
         surface_evolution.visible = storage.nauvis_lab_exploded or false
-
-        warpflow.add{type="label", caption="Auto Warp: "}
-        warpflow.add{type="label", name="variable", caption="∞"}
         warpflow.visible = storage.timer.active
-
-        manualwarpflow.add{type="label", caption="Manual Warp: "}
-        manualwarpflow.add{type="label", name="variable", caption="∞"}
         manualwarpflow.visible = storage.timer.active
 
         warp_frame.add{type="button", name="warp_button", caption={"dw-gui.warp-button"}, style = "confirm_button"}
@@ -96,7 +81,7 @@ local function warp_frame_click(event)
 end
 
 local function prepare_warp_gui(player)
-    dw.gui.get_warp_toggle_button(player)
+    dw.gui.set_warp_toggle_buttons(player)
     dw.gui.get_warp_frame(player)
 end
 
@@ -122,20 +107,24 @@ dw.update_gui = function()
         frame.surface_time.visible = storage.nauvis_lab_exploded or false
         frame.surface_evolution.visible = storage.nauvis_lab_exploded or false
 
-        frame.surface.variable.caption = {"space-location-name." .. storage.warp.current.type}
-        frame.dimension.variable.caption = storage.warp.number
-        frame.surface_evolution.variable.caption = string.format("%.2f %%", game.forces.enemy.get_evolution_factor(storage.warp.current.surface) * 100)
-        frame.surface_time.variable.caption = utils.format_time(game.tick/60 - storage.warp.time/60)
+        frame.surface.caption = {"dw-gui.planet", storage.warp.current.type, {"space-location-name." .. storage.warp.current.type}}
+        frame.dimension.caption = {"dw-gui.dimension", storage.warp.number}
+        frame.surface_evolution.caption = {"dw-gui.planet_evolution", string.format("%.2f", game.forces.enemy.get_evolution_factor(storage.warp.current.surface) * 100)}
+        frame.surface_time.caption = {"dw-gui.planet_clock", utils.format_time(game.tick/60 - storage.warp.time/60)}
 
         if storage.timer.active then
             if storage.timer.warp >= 0 then
                 frame.warp_timer.visible = true
-                frame.warp_timer.variable.caption = utils.format_time(storage.timer.warp)
+                local timer = utils.format_time(storage.timer.warp)
+                if storage.timer.warp <= 60 then timer = "[font=default-bold][color=#faf17a]" .. timer .. "[/color][/font]" end
+                frame.warp_timer.caption = {"dw-gui.autowarp_timer", timer}
             else
                 frame.warp_timer.visible = false
             end
             frame.warp_timer_manual.visible = true
-            frame.warp_timer_manual.variable.caption = utils.format_time(storage.timer.manual_warp)
+            local timer = utils.format_time(storage.timer.manual_warp)
+            if storage.timer.manual_warp < 10 then timer = "[font=default-bold][color=#faf17a]" .. timer .. "[/color][/font]" end
+            frame.warp_timer_manual.caption = {"dw-gui.manualwarp_timer", timer}
         end
     end
 end
