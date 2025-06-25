2b09820aa43d8e50160e1e89fd41facf29b90e21
Kyria <misc+gh@khon.dev>
2025-06-26 00:24:41 +0200

    feat: add early mining productivity and adjust others

--- prototypes/technologies/space-age/others/mining.lua
+++ prototypes/technologies/space-age/others/mining.lua
@@ -1,8 +1,8 @@
 
 -- fix mining productivity in space age
-data.raw['technology']['dimension-mining-productivity-16'] = nil
+data.raw['technology']['dimension-mining-productivity-21'] = nil
 
-local mining_prod = data.raw['technology']['dimension-mining-productivity-11']
-mining_prod.max_level = 20
-mining_prod.prerequisites = {"mining-productivity-3", "dimension-mining-productivity-6"}
-mining_prod.unit.count_formula = "1000+500*(L-10)"
+local mining_prod = data.raw['technology']['dimension-mining-productivity-16']
+mining_prod.max_level = 25
+mining_prod.prerequisites = {"mining-productivity-3", "dimension-mining-productivity-11"}
+mining_prod.unit.count_formula = "1000+500*(L-15)"
