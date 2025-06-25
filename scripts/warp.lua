6ff1a789287a1ad2206d00d0c0a85210f289741b
Kyria <misc+gh@khon.dev>
2025-06-26 00:21:10 +0200

    fix: update manual warp time to be more smooth until jump 100 (~=15min)

--- scripts/warp.lua
+++ scripts/warp.lua
@@ -5,7 +5,7 @@ local function calculate_manual_warp_time()
     local base_time = 10 --seconds
     local max_time = settings.global['dw-manual-warp-max-time'].value * 60
     local warp_zone = math.floor(storage.warp.number * settings.global['dw-manual-warp-zone-multiplier'].value)
-    local warp_multi = math.floor((1 + (math.min(100, storage.warp.number)) / 5) ^ (2 + (math.min(100, storage.warp.number) / 40)))
+    local warp_multi = math.floor((1 + (math.min(150, storage.warp.number)) / 10) ^ (2 + (math.min(150, storage.warp.number) / 100)))
 
     return math.min(max_time, base_time + warp_zone + warp_multi)
 end
