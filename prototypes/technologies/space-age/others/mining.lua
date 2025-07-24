06b81d36a08df87f4a4fc58770d76337a45006a4
Kyria <misc+gh@khon.dev>
2025-07-24 02:57:14 +0200

    fix: tech prereq not working

--- prototypes/technologies/space-age/others/mining.lua
+++ prototypes/technologies/space-age/others/mining.lua
@@ -4,5 +4,5 @@ data.raw['technology']['dimension-mining-productivity-21'] = nil
 
 local mining_prod = data.raw['technology']['dimension-mining-productivity-16']
 mining_prod.max_level = 25
-mining_prod.prerequisites = {"mining-productivity-3", "dimension-mining-productivity-11"}
+mining_prod.prerequisites = {"production-science-pack", "dimension-mining-productivity-11"}
 mining_prod.unit.count_formula = "1000+500*(L-15)"
