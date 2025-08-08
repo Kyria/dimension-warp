a767e7568458493af5441b1de38eb07b0c855165
Kyria <misc+gh@khon.dev>
2025-08-08 03:04:48 +0200

    feat: add migration to make sure user frames are resetted once updated

--- migrations/0.3.5.update_gui.lua
+++ migrations/0.3.5.update_gui.lua
@@ -1,3 +1,4 @@
+--- As the GUI frame change in 0.3.5, destroy it first if it exists so we have a new one.
 for _, player in pairs(game.players) do
     local frameflow = mod_gui.get_frame_flow(player)
     if frameflow.warp_frame then frameflow.warp_frame.destroy() end
