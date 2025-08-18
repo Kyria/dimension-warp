49e72433f30668345453d9f8264084297a8adcbe
Kyria <misc+gh@khon.dev>
2025-08-18 23:44:10 +0200

    balance: changed pipe max extent  to most entities with fluidbox

--- prototypes/updates/pipes.lua
+++ prototypes/updates/pipes.lua
@@ -1,16 +1,23 @@
 -- force all pipes entity and storage tanks to have long extent, to avoid issue due to mobile gates / harvesters
-for _, pipe_entity in pairs(data.raw['pipe']) do
-    if pipe_entity.fluid_box then
-        pipe_entity.fluid_box.max_pipeline_extent = 25000
-    end
-end
-for _, pipe_entity in pairs(data.raw['pipe-to-ground']) do
-    if pipe_entity.fluid_box then
-        pipe_entity.fluid_box.max_pipeline_extent = 25000
-    end
-end
-for _, tank in pairs(data.raw['storage-tank']) do
-    if tank.fluid_box then
-        tank.fluid_box.max_pipeline_extent = 25000
+local fluid_box_entity_types = {'valve', 'pump', 'pipe', 'pipe-to-ground', 'storage-tank', 'mining-drill', 'offshore-pump', 'assembling-machine', 'boiler', 'fluid-turret'}
+for _, entity_type in pairs(fluid_box_entity_types) do
+    for _, entity in pairs(data.raw[entity_type]) do
+        -- most entities
+        if entity.fluid_box then
+            entity.fluid_box.max_pipeline_extent = 25000
+        end
+        -- drills, boiler (output)
+        if entity.input_fluid_box then
+            entity.input_fluid_box.max_pipeline_extent = 25000
+        end
+        if entity.output_fluid_box then
+            entity.output_fluid_box.max_pipeline_extent = 25000
+        end
+        -- assembling machine
+        if entity.fluid_boxes then
+            for _, fluid_box in pairs(entity.fluid_boxes) do
+                fluid_box.max_pipeline_extent = 25000
+            end
+        end
     end
 end
