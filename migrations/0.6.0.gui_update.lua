1c1af05c66c52c6ec0b76b5ae2f1f48cf395bb3c
Kyria <github@khon.dev>
2025-12-24 23:00:21 +0100

    feat: add migration file to force the GUI updates as expected

---
+++ migrations/0.6.0.gui_update.lua
@@ -0,0 +1,8 @@
+--- Force GUI recreation to make sure we have everything in the right order.
+storage.planet_selector_enabled = false
+storage.planet_selector_list = {}
+storage.warp.preferred_destination = nil
+for _, player in pairs(game.players) do
+    local frameflow = mod_gui.get_frame_flow(player)
+    if frameflow.warp_frame then frameflow.warp_frame.destroy() end
+end
