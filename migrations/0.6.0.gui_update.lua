a11aa725d09f3b65c7490f75ad4f101906e8bcd2
Kyria <github@khon.dev>
2025-12-25 03:10:12 +0100

    fix: migration script not checking the right GUI children

--- migrations/0.6.0.gui_update.lua
+++ migrations/0.6.0.gui_update.lua
@@ -4,5 +4,5 @@ storage.planet_selector_list = {}
 storage.warp.preferred_destination = nil
 for _, player in pairs(game.players) do
     local frameflow = mod_gui.get_frame_flow(player)
-    if frameflow.warp_frame then frameflow.warp_frame.destroy() end
+    if frameflow.dw_frame and frameflow.dw_frame.warp_frame then frameflow.dw_frame.warp_frame.destroy() end
 end
